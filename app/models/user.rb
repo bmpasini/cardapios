ff32afe0cadc464acd7fb51316c6fb284969d6c9
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-10-02 13:27:40 -0400

    added user validations and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :favorite_restaurants, class_name: "Restaurant", foreign_key: :customer_id
 	has_many :photos, as: :imageable
+	
+	validates :name, presence: true
+	validates :email, presence: true
+  validates :password, presence: true
+  validates_uniqueness_of :email
+  validates_confirmation_of :password, message: "Password doesn't match"
+  
+  has_secure_password
 end
--- db/migrate/20141001154410_create_users.rb
+++ db/migrate/20141001154410_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
+      t.string :name, null: false 
+      t.string :email, null: false 
+      t.string :password, null: false 
       t.string :photo
       t.boolean :is_admin?
 
