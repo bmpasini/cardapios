a58c5adb08c432689bc35b84678eb8da1b5f3b82
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-11-07 23:32:15 -0500

    removed sort by roved restful routes of sort by

--- app/controllers/sorts_controller.rb
+++ app/controllers/sorts_controller.rb
@@ -1,14 +1,14 @@
 class SortsController < ApplicationController
-	def index
-		@restaurants = Restaurant.all
-		@specialties = RestaurantSpecialty.all
-		@neighborhoods = Neighborhood.all
-	end
+	# def index
+	# 	@restaurants = Restaurant.all
+	# 	@specialties = RestaurantSpecialty.all
+	# 	@neighborhoods = Neighborhood.all
+	# end
 
-	def show_specialty
-		@specialty = RestaurantSpecialty.find(params[:id])
-		@restaurants = @specialty.restaurants
-	end
+	# def show_specialty
+	# 	@specialty = RestaurantSpecialty.find(params[:id])
+	# 	@restaurants = @specialty.restaurants
+	# end
 
 	def show_neighborhood
 		@neighborhood = Neighborhood.find(params[:id])
