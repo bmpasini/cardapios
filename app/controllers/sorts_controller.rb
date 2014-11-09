8454691b9be72b1dd47426d971d1a560663d75b7
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-11-08 23:42:51 -0500

    fixed sorts view

--- app/controllers/sorts_controller.rb
+++ app/controllers/sorts_controller.rb
@@ -1,9 +1,9 @@
 class SortsController < ApplicationController
-	# def index
-	# 	@restaurants = Restaurant.all
-	# 	@specialties = RestaurantSpecialty.all
-	# 	@neighborhoods = Neighborhood.all
-	# end
+	def index
+		@restaurants = Restaurant.all
+		@specialties = RestaurantSpecialty.all
+		@neighborhoods = Neighborhood.all
+	end
 
 	# def show_specialty
 	# 	@specialty = RestaurantSpecialty.find(params[:id])
